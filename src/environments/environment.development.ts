9c72c1230ad2939aebefd24dfd1c0640903dfbe0
Max-Lev <maxlevtov@gmail.com>
2025-01-12 14:02:05 +0200

    db path rename

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     development:true,
-    getUsersApi:'http://localhost:4200/assets/db/usersDB.json',
-    getOrdersApi:'http://localhost:4200/assets/db/ordersDB.json'
+    getUsersApi:'http://localhost:4200/assets/data-base/usersDB.json',
+    getOrdersApi:'http://localhost:4200/assets/data-base/ordersDB.json'
 };
